8513d9e967160b914ee7a878b465a1fddc4d4bb9
mhmd329 <mhmdkhaled092@gmail.com>
2025-02-12 00:03:19 +0200

    fix packages instalation

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react'
 import Comments from '../FakeApis/CommentData'
  const Comment = ({postId}) => {
-    const [comments,setComments] = useState(Comments)
   return (
     <div className="mt-4 border-t pt-3">
     <h3 className="text-md font-semibold text-gray-700">Comments for Post {postId}</h3>
     <div className="mt-2 space-y-4">
-      {comments.map((comment) => (
+      {Comments.map((comment) => (
         <div key={comment.id} className="flex  items-start space-x-4">
           {/* Comment Profile Image */}
           <img
